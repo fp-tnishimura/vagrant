b39f6fbe4546f3765ab2ca82e6ba8ee677c3aeb1
fp-tnishimura <nishimura_tomoaki@freeplus.co.jp>
2017-07-07 17:18:20 +0900

    modified ruby version

--- provision/scripts/provision.sh
+++ provision/scripts/provision.sh
@@ -6,5 +6,5 @@ echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 source ~/.bash_profile
 
-rbenv install 2.2.2
-rbenv global 2.2.2
+rbenv install 2.2.0
+rbenv global 2.2.0
